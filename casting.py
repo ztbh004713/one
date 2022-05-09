47276a55a135ed5b37cfffb0709ea2204c1abc29
Ayton <a22980688@gmail.com>
2022-05-10 02:13:12 +0800

    Add new file

--- casting.py
+++ casting.py
@@ -1,9 +1,9 @@
 age = input("請輸入您的年齡: ")
 age = int(age)
-if age >= 18 and age <20:
+if age >= 18 and age < 20:
 	print("刑法已成年")
-elif age >=20:
-	print("民法已成年")
+elif age >=20 and age < 30:
+	print("民法已成年`,可以參選民代")
 elif age >= 30 and age < 55:
 	print("正值青壯年")
 elif age >= 55:
