f1e221b546fdbf7b284991197b896f77d2d1d7ec
Ayton <a22980688@gmail.com>
2022-05-22 04:42:57 +0800

    new

--- continue.py
+++ continue.py
@@ -7,7 +7,7 @@
 products = [] 
 with open("garbled.csv" , "r" , encoding="utf-8") as f:  
 	for line in f:
-		if "商品,价格,糖分,容量" in line:
+		if "商品,价格,糖分,容量" in line:							#if判断，并撰写条件确认
 			continue 											#有继续的意思，在执行for回圈时，如果有碰到撰写的条件时，则后续程式码不执行，直接跳到下一回圈
 		name, price, sugar, capacity = line.strip().split(",")                     
 		products.append([name , price , sugar , capacity])
